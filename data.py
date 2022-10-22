c87cdc453e83c045b37b01108d0390cba7aac4c6
Nathan Grilliot <nrgrill2003@gmail.com>
2022-10-22 17:09:27 -0400

    Added Submit

--- data.py
+++ data.py
@@ -12,11 +12,11 @@ def get_elections():
         error = f"{type(e).__name__} exception: {e.args!r}"
         return error
 
-def get_representatives():
+def get_representatives(address):
         try:
             url = "https://www.googleapis.com/civicinfo/v2/representatives"
             headers = {'X-goog-api-key': 'AIzaSyCJvZXYJQQlgH2YzinPMUpbD22NbKiD36k'}
-            query = {'address': '3038 Taylor Ave Cincinnati OH'}
+            query = {'address': address}
             req = requests.get(url, headers=headers, params=query)
 
             return req.json()
@@ -27,3 +27,4 @@ def get_representatives():
 
 data = get_elections()
 data = get_representatives()
+address = userInput
