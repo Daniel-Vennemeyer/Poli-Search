a15ae4405f656a9999997c6e704a4231e69e1fc9
Nathan Grilliot <nrgrill2003@gmail.com>
2022-10-23 04:57:48 -0400

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -22,12 +22,16 @@ def information():
     party = data_retrieval.get_party(official)
     info = data_retrieval.get_wiki_info(user_input)
     image = data_retrieval.get_image_name(party)
-    print(user_input)
-    print(officials_list)
-    print(official)
-    print(party)
-    print(info)
-    return render_template("information.html", title="Information", user_input=user_input, info=info, party=party, official=official, image=image)
+    id = data_retrieval.get_candidate_id(user_input)
+    news = data_retrieval.get_committees(id)
+    print(news)
+    #print(user_input)
+    #print(officials_list)
+    #print(official)
+    #print(party)
+    #print(info)
+    return render_template("information.html", title="Information", user_input=user_input, info=info, 
+    party=party, official=official, image=image, news=news)
 
 
 @bp.route("/survey", methods=["GET", "POST"])
