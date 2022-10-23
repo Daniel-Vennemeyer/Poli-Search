354df1a947252e6f1c634f1e9f30839f750cc85c
Daniel Vennemeyer <77549269+Daniel-Vennemeyer@users.noreply.github.com>
2022-10-22 22:40:56 -0400

    Added tons of stuff to wiki data

--- data_retrieval.py
+++ data_retrieval.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth
 from difflib import SequenceMatcher
+import wikipedia
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
@@ -21,21 +22,29 @@ def get_officials():
     try:
         url = "https://www.googleapis.com/civicinfo/v2/representatives"
         headers = {'X-goog-api-key': 'AIzaSyCJvZXYJQQlgH2YzinPMUpbD22NbKiD36k'}
-        query = {'address': '3038 Taylor Ave Cincinnati OH'}
-        req = requests.get(url, headers=headers, params=query)
+        addresses = ['45040','Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois', 'Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana', 'Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Oklahoma','Oregon','Pennsylvania', 'Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
 
-        return req.json()["officials"]
+        officials = []
+        for address in addresses:
+            query = {'address': address}
+            req = requests.get(url, headers=headers, params=query)
+            if "officials" in req.json():
+                for official in req.json()["officials"]:
+                    if official not in officials:
+                        officials.append(official)
+        return officials
     except Exception as e:
         error = f"{type(e).__name__} exception: {e.args!r}"
         return error
 
-def search_officials(name):
+def search_officials(name, officials):
     try:
-        officials = get_officials()
         sim_list = []
         for official in officials:
             sim_list.append((official['name'],similar(official['name'], name)))
-            if similar(official['name'], name) > .5:
+            if similar("joe biden", name) > .8:
+                name = "joseph r biden"
+            if similar(official['name'], name) > .7:
                 return official
     except Exception as e:
         error = f"{type(e).__name__} exception: {e.args!r}"
@@ -47,26 +56,39 @@ def get_official_photo(official):
 def get_party(official):
     return official['party'] if 'party' in official else "NO PARTY"
 
-def get_wiki_info(official):
+def get_wiki_info(name):
     try:
-        title = ''
-        urls = official['urls'] if 'urls' in official else ""
-        if urls:
-            for url in urls:
-                wiki_url = url if "wikipedia" in url else ""
-        if wiki_url:
-            response = requests.get(url=wiki_url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            # soup = BeautifulSoup(response.content, 'lxml')
-            title = soup.find(id="firstHeading")
+        # if official:
+            # urls = official['urls'] if 'urls' in official else ""
+            # if urls:
+            #     for url in urls:
+            #         wiki_url = url if "wikipedia" in url else ""
+            # if wiki_url:
+            #     response = requests.get(url=wiki_url)
+            #     title = BeautifulSoup(response.content, 'html.parser').find(id="firstHeading").string
 
+        title = wikipedia.search(name, results=1)[0]
+        if title.lower() == "joe biden":
+            title = "joe bide"
 
-        return title.string
+        summary = wikipedia.summary(title)
+        content = wikipedia.page(title).content.split("==")
+
+        heading = ""
+        for section in content:
+            if 'Political positions' in section:
+                heading = section
+        if heading:
+            positions = content[content.index(heading)+1] if content[content.index(heading)+1] else content[content.index(heading)+2]
+        else:
+            positions = ""
+
+        return (summary, positions)
     except Exception as e:
         error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
+        return 'POLITICIAN NOT FOUND'
 
-data = get_elections()
-data = get_officials()
-official = search_officials("joe biden")
-info = get_wiki_info(official)
+
+officials_list = get_officials()
+# official = search_officials("joe biden", officials_list)
+info = get_wiki_info("Joe Biden")
