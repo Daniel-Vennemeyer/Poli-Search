572fdd8a655cf04c4f1990fbbfc2389cf6eabdc0
Daniel Vennemeyer <77549269+Daniel-Vennemeyer@users.noreply.github.com>
2022-11-02 14:51:47 -0400

    Completely refactored data retrieval

--- app/main/data_retrieval.py
+++ app/main/data_retrieval.py
@@ -1,171 +1,117 @@
 import requests
-from difflib import SequenceMatcher
-import wikipedia
+import wikipediaapi
 import flask
 
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
 
-def get_elections():
-    try:
-        url = "https://www.googleapis.com/civicinfo/v2/elections"
-        headers = {'X-goog-api-key': 'AIzaSyCJvZXYJQQlgH2YzinPMUpbD22NbKiD36k'}
-        req = requests.get(url, headers=headers)
+def get_sim_value(survey_result="Survey result - N/A"):
+    return survey_result
 
-        return req.json()["elections"]
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
 
-def get_officials():
-    try:
-        url = "https://www.googleapis.com/civicinfo/v2/representatives"
-        headers = {'X-goog-api-key': 'AIzaSyCJvZXYJQQlgH2YzinPMUpbD22NbKiD36k'}
-        #addresses = ['45040','Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois', 'Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana', 'Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Oklahoma','Oregon','Pennsylvania', 'Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
-        addresses = ['45040']
-        officials = []
-        for address in addresses:
-            query = {'address': address}
-            req = requests.get(url, headers=headers, params=query)
-            if "officials" in req.json():
-                for official in req.json()["officials"]:
-                    if official not in officials:
-                        officials.append(official)
-        return officials
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
+class official():
+    def __init__(self, name):
+        self.name = name
+        self.openfec_key = 'szr3iTkQTg9eZYOhFwvWKB49mFlACXOzqMV7uJut'
+        self.id = self.get_candidate_id()
+        self.committee_id = self.get_committee_id()
+        self.history = self.get_history(self.id)
+        self.party = self.get_party()
 
-def search_officials(name, officials):
-    try:
-        sim_list = []
-        for official in officials:
-            sim_list.append((official['name'],similar(official['name'], name)))
-            if similar("joe biden", name) > .8:
-                name = "joseph r biden"
-            if similar(official['name'], name) > .7:
-                return official
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
+    def get_wiki_info(self):
+        try:
+            wiki = wikipediaapi.Wikipedia('en', extract_format=wikipediaapi.ExtractFormat.WIKI)
+            page = wiki.page(self.name)
+            self.name = page.title
 
-def get_official_photo(official):
-    return official['photoUrl'] if 'photoUrl' in official else "NO PHOTO"
+            summary = page.summary
 
-def get_party(official):
-    return official['party'] if 'party' in official else "NO PARTY"
+            heading = ""
+            for section in page.sections:
+                if 'Political positions' in section.title:
+                    heading = page.sections[page.sections.index(section) + 1].title
+            positions = page.text.split("Political positions")[1].split(heading)[0]
 
-def get_wiki_info(name):
-    try:
-        # if official:
-            # urls = official['urls'] if 'urls' in official else ""
-            # if urls:
-            #     for url in urls:
-            #         wiki_url = url if "wikipedia" in url else ""
-            # if wiki_url:
-            #     response = requests.get(url=wiki_url)
-            #     title = BeautifulSoup(response.content, 'html.parser').find(id="firstHeading").string
+            return (summary, positions)
+        except Exception as e:
+            error = f"{type(e).__name__} exception: {e.args!r}"
+            return (error, 'POLITICIAN NOT FOUND')
 
-        title = wikipedia.search(name, results=1)[0]
-        if title.lower() == "joe biden":
-            title = "joe bide"
+    def get_name(self):
+        return self.name
 
-        summary = wikipedia.summary(title)
-        content = wikipedia.page(title).content.split("==")
-
-        heading = ""
-        for section in content:
-            if 'Political positions' in section:
-                heading = section
-        if heading:
-            positions = content[content.index(heading)+1] if content[content.index(heading)+1] else content[content.index(heading)+2]
+    def get_image_name(self):
+        if self.party.lower() == 'republican party':
+            image = flask.url_for('static', filename='republican.png')
         else:
-            positions = ""
+            image = flask.url_for('static', filename='democratic.png')
+        return image
 
-        return (summary, positions)
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return 'POLITICIAN NOT FOUND'
+    def get_candidate_id(self):
+        try:
+            url = f'https://api.open.fec.gov/v1/names/candidates/?api_key={self.openfec_key}&q={self.name.replace(" ", "%20")}'
+            req = requests.get(url)
+            return req.json()["results"][0]['id']
+        except Exception as e:
+            error = f"{type(e).__name__} exception: {e.args!r}"
+            return error
 
-def get_image_name(party):
-    if party == 'Republican Party':
-        image = flask.url_for('static', filename='republican.png')
-    else:
-        image = flask.url_for('static', filename='democratic.png')
-    return image
+    def get_committees(self, id):
+        try:
+            url = f"https://api.open.fec.gov/v1/candidate/{id}/committees/?page=1&per_page=20&sort_null_only=false&api_key={self.openfec_key}&sort_nulls_last=false&sort_hide_null=false&sort=name"
+            req = requests.get(url)
+            return req.json()['results'][0]['name']
+        except Exception as e:
+            error = f"{type(e).__name__} exception: {e.args!r}"
+            return error
 
-def get_sim_value(surveyresult = "Survey result - N/A"):
-    return surveyresult
+    def get_filings(self, id):
+        try:
+            url = f"https://api.open.fec.gov/v1/candidate/{id}/filings/?page=1&sort_nulls_last=false&sort=-receipt_date&per_page=20&sort_null_only=false&api_key={self.openfec_key}&sort_hide_null=false"
+            req = requests.get(url)
+            return req.json()['results'][0]
+        except Exception as e:
+            error = f"{type(e).__name__} exception: {e.args!r}"
+            return error
 
-def get_candidate_id(name):
-    try:
-        key= "szr3iTkQTg9eZYOhFwvWKB49mFlACXOzqMV7uJut"
-        fname,lname=name.split(" ")
-        url = f"https://api.open.fec.gov/v1/names/candidates/?api_key={key}&q={fname}%20{lname}"
-        req = requests.get(url)
-        return req.json()["results"][0]['id']
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
+    def get_committee_id(self):
+        try:
+            url = f"https://api.open.fec.gov/v1/names/committees/?api_key={self.openfec_key}&q={self.name.replace(' ', '%20')}"
+            req = requests.get(url)
+            return req.json()['results'][0]['id']
+        except Exception as e:
+            error = f"{type(e).__name__} exception: {e.args!r}"
+            return error
 
-def get_committees(id):
-    try:
-        key= "szr3iTkQTg9eZYOhFwvWKB49mFlACXOzqMV7uJut"
-        url = f"https://api.open.fec.gov/v1/candidate/{id}/committees/?page=1&per_page=20&sort_null_only=false&api_key={key}&sort_nulls_last=false&sort_hide_null=false&sort=name"
-        req = requests.get(url)
-        return req.json()['results'][0]['name']
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
+    def get_history(self, id):
+        try:
+            url = f"https://api.open.fec.gov/v1/candidate/{id}/?page=1&sort_nulls_last=false&sort=name&per_page=20&sort_null_only=false&api_key={self.openfec_key}&sort_hide_null=false"
+            req = requests.get(url)
+            return req.json()['results'][0]
+        except Exception as e:
+            error = f"{type(e).__name__} exception: {e.args!r}"
+            return error
 
-def get_filings(id):
-    try:
-        key= "szr3iTkQTg9eZYOhFwvWKB49mFlACXOzqMV7uJut"
-        url = f"https://api.open.fec.gov/v1/candidate/{id}/filings/?page=1&sort_nulls_last=false&sort=-receipt_date&per_page=20&sort_null_only=false&api_key={key}&sort_hide_null=false"
-        req = requests.get(url)
-        return req.json()['results'][0]
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
+    def get_party(self):
+        return self.history['party_full'] if 'party_full' in self.history else "PARTY NOT FOUND"
 
-def get_committee_id(id):
-    try:
-        name = get_committees(id)
-        key= "szr3iTkQTg9eZYOhFwvWKB49mFlACXOzqMV7uJut"
-        url = f"https://api.open.fec.gov/v1/names/committees/?api_key={key}&q={name.replace(' ', '%20')}"
-        req = requests.get(url)
-        return req.json()['results'][0]['id']
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
-
-def get_history(id):
-    try:
-        key= "szr3iTkQTg9eZYOhFwvWKB49mFlACXOzqMV7uJut"
-        url = f"https://api.open.fec.gov/v1/candidate/{id}/?page=1&sort_nulls_last=false&sort=name&per_page=20&sort_null_only=false&api_key={key}&sort_hide_null=false"
-        req = requests.get(url)
-        return req.json()['results'][0]
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
-
-def get_finances(name):
-    try:
-        committee_id = get_committee_id(get_candidate_id("Robert Portman"))
-        key= "szr3iTkQTg9eZYOhFwvWKB49mFlACXOzqMV7uJut"
-        url = f"https://api.open.fec.gov/v1/committee/{committee_id}/totals/?api_key={key}&sort_nulls_last=false&page=1&sort_hide_null=false&per_page=20&sort=-cycle&sort_null_only=false"
-        req = requests.get(url)
-        finances = req.json()['results'][0]
-        fin_str = ""
-        for item in finances:
-            fin_str += f"{item}: {finances[item]}\n"
-        return fin_str
-    except Exception as e:
-        error = f"{type(e).__name__} exception: {e.args!r}"
-        return error
+    def get_finances(self):
+        try:
+            committee_id = official.get_committee_id(official.get_candidate_id(self.name))
+            url = f"https://api.open.fec.gov/v1/committee/{committee_id}/totals/?api_key={self.openfec_key}&sort_nulls_last=false&page=1&sort_hide_null=false&per_page=20&sort=-cycle&sort_null_only=false"
+            req = requests.get(url)
+            finances = req.json()['results'][0]
+            fin_str = ""
+            for item in finances:
+                fin_str += f"{item}: {finances[item]}\n"
+            return fin_str
+        except Exception as e:
+            error = f"{type(e).__name__} exception: {e.args!r}"
+            return error
 
 
-id = get_candidate_id("Robert Portman")
-info = get_committee_id(id)
-# finances = get_finances(info)
-finances = get_finances("Robert Portman")
+official = official("Bernie Sanders")
+# info = official.get_wiki_info()
+# id = official.get_candidate_id("Bernie Sanders")
+# info = official.get_committee_id(id)
+# info = official.get_history(id)
+# info = official.get_party_v2(id)
+# finances = official.get_finances("Robert Portman")
--- app/main/routes.py
+++ app/main/routes.py
@@ -4,83 +4,106 @@ from flask_wtf.csrf import CSRFError
 from werkzeug.exceptions import HTTPException
 
 from app.main import bp
-from app.main.forms import CookiesForm
 from app.main import data_retrieval
 
 global user_input
 
+
 @bp.route("/", methods=["GET", "POST"])
 def index():
+    # if request.method == "POST":
+    #     session["user_input"] = request.form['user-input']
     return render_template("main/index.html")
 
 
+# @bp.route("/information", methods=["GET", "POST"])
 @bp.route("/information", methods=["POST"])
 def information():
-    user_input = request.form['user-input']
-    officials_list = data_retrieval.get_officials()
-    official = data_retrieval.search_officials(user_input, officials_list)
-    party = data_retrieval.get_party(official)
-    info = data_retrieval.get_wiki_info(user_input)
-    image = data_retrieval.get_image_name(party)
-    finances = data_retrieval.get_finances(user_input)
-    #print(user_input)
-    #print(officials_list)
-    #print(official)
-    #print(party)
-    #print(info)
-    return render_template("information.html", title="Information", user_input=user_input, info=info, 
-    party=party, official=official, image=image, finances=finances, result=session['result'])
+    user_input = "Joe Biden"
+
+    official = data_retrieval.official(user_input)
+    party = official.party
+    image = official.get_image_name()
+    info = official.get_wiki_info()
+    finances = official.get_finances()
+    return render_template("information.html", title="Information", user_input=user_input, info=info,
+                           party=party, official=official, image=image, finances=finances, result="")
+
+    if request.method == "POST":
+        user_input = request.form['user-input']
+        # user_input = request.form.get('user-input')
+        # user_input = session["user_input"]
+        # user_input = "Joe Biden"
+
+        official = data_retrieval.official(user_input)
+        party = official.party
+        image = official.get_image_name()
+        info = official.get_wiki_info()
+        finances = official.get_finances()
+        return render_template("information.html", title="Information", user_input=user_input, info=info,
+                               party=party, official=official, image=image, finances=finances, result=result)
+    else:
+        return render_template("information.html", title="", user_input="", info="",
+                           party="", official="", image="", finances="", result="")
 
 
 @bp.route("/survey", methods=["GET", "POST"])
 def survey():
-    demo_count, repub_count = 0, 0
-    if request.method == "POST": 
-        pol_stance = request.form.get('pol-stance')
-        if pol_stance == "Democratic":
-            demo_count += 1
+    # demo_count, repub_count = 0, 0
+    if request.method == "POST":
+        dem_count = 0
+        dem_count += 1 if request.form.get('pol-stance') == "Democratic" else 0
+        dem_count += 1 if request.form.get('abortion') == "Pro-choice" else 0
+        dem_count += 1 if request.form.get('fiscally') == "Fiscally-liberal" else 0
+        dem_count += 1 if request.form.get('gun_law') == "Yes" else 0
+        dem_count += 1 if request.form.get('LGBTQ') == "Yes" else 0
+        # pol_stance = request.form.get('pol-stance')
+        # if pol_stance == "Democratic":
+        #     demo_count += 1
+        # else:
+        #     repub_count += 1
 
+        # abortion = request.form.get('abortion')
+        # if abortion == "Pro-choice":
+        #     demo_count += 1
+        # else:
+        #     repub_count += 1
+
+        # fiscally = request.form.get('fiscally')
+        # if fiscally == "Fiscally-liberal":
+        #     demo_count += 1
+        # else:
+        #     repub_count += 1
+
+        # gun_law = request.form.get('gun_law')
+        # if gun_law == "Yes":
+        #     demo_count += 1
+        # else:
+        #     repub_count += 1
+
+        # lgbtq = request.form.get('LGBTQ')
+        # if lgbtq == "Yes":
+        #     demo_count += 1
+        # else:
+        #     repub_count += 1
+
+        # if demo_count > repub_count:
+        #     result = (f"You are {(demo_count * 100 // 5)}% Democratic")
+        # else:
+        #     result = (f"You are {(repub_count * 100 // 5)}% Republican")
+
+        if dem_count > 3:
+            result = (f"You are {(dem_count * 100 // 5)}% Democratic")
         else:
-            repub_count += 1    
+            result = (f"You are {(100 - (dem_count * 100 // 5))}% Republican")
 
-        abortion = request.form.get('abortion')
-        if abortion == "Pro-choice":
-            demo_count += 1
-
-        else:
-            repub_count += 1 
-
-        fiscally = request.form.get('fiscally')
-        if fiscally == "Fiscally-liberal":
-            demo_count += 1
-
-        else:
-            repub_count += 1 
-
-        gun_law = request.form.get('gun_law')
-        if gun_law == "Yes":
-            demo_count += 1
-
-        else:
-            repub_count += 1 
-
-        lgbtq = request.form.get('LGBTQ')
-        if lgbtq == "Yes":
-            demo_count += 1
-
-        else:
-            repub_count += 1 
-
-        if demo_count > repub_count:
-            result = (f"You are {(demo_count*100//5)}% Democratic")
-        else:
-            result = (f"You are {(repub_count*100//5)}% Republican")
         session['result'] = result
 
     return render_template("survey.html", title="Survey")
-    
 
-@bp.route("/about_us", methods=["GET", "POST"])
+
+# @bp.route("/about_us", methods=["GET", "POST"])
+@bp.route("/about_us", methods=["GET"])
 def about_us():
     return render_template("about_us.html", title="About Us")
 
